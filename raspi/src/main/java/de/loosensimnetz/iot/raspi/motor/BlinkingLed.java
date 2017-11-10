2f0d116add6617e76dcf5c25b07419b28e83000c
Joerg Loosen <joerg-loosen@gmx.de>
2017-11-10 16:56:46 +0100

    Implement turning on / off leds according to motor state.

---
+++ raspi/src/main/java/de/loosensimnetz/iot/raspi/motor/BlinkingLed.java
@@ -0,0 +1,139 @@
+package de.loosensimnetz.iot.raspi.motor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.loosensimnetz.iot.raspi.motor.Motor.LedState;
+
+/**
+ * Daemon thread to make the leds blink a few times.
+ * 
+ * Two modes: ALTERNATING means led1 = ON, led2 = OFF, then toggle SIMULTANEAOUS
+ * means led1 = ON, led2 = ON, then toggle
+ * 
+ * @author jloosen
+ *
+ */
+public class BlinkingLed extends Thread {
+	private final int numBlink;
+	private final long delay;
+	private final BlinkType blinkType;
+	private final MotorSensor motorSensor;
+
+	private LedState ledState12, ledState13;
+
+	private final static Logger logger = LoggerFactory.getLogger(BlinkingLed.class);
+
+	/**
+	 * Blinking mode - alternating vs. simultaneous mode
+	 * 
+	 * @author jloosen
+	 *
+	 */
+	public enum BlinkType {
+		ALTERNATING, SIMULTANEOUS
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param numBlink
+	 *            Number of times the leds blink. Zero means blinking until thread
+	 *            is interrupted. Must be greater than or equal to zero.
+	 * @param delay
+	 *            Delay in milliseconds between the toggling. Must be greater than
+	 *            or equal to zero.
+	 * @param blinkType
+	 *            Mode = ALTERNATING or SIMULTANEOUS
+	 * @param motorSensor
+	 *            The motor sensor
+	 */
+	public BlinkingLed(int numBlink, long delay, BlinkType blinkType, MotorSensor motorSensor) {
+		super();
+		this.numBlink = numBlink;
+		this.delay = delay;
+		this.blinkType = blinkType;
+		this.motorSensor = motorSensor;
+		this.setDaemon(true);
+		this.setName("Blinking led daemon thread");
+
+		if (numBlink < 0)
+			throw new IllegalArgumentException("Parameter numBlink must be greater than or eaqual to zero!");
+		if (delay < 0)
+			throw new IllegalArgumentException("Parameter numBlink must be greater than or eaqual to zero!");
+	}
+
+	/**
+	 * Initialize the leds according to the BlinkingMode. Then toggle the leds
+	 * according to the values provided.
+	 * 
+	 */
+	@Override
+	public void run() {
+		logger.info("Starting to blink {} times. Mode: {}, delay: {}");
+		int round = 1;
+		boolean goOn = true;
+
+		switch (blinkType) {
+		case ALTERNATING:
+			initAlternating();
+			break;
+		case SIMULTANEOUS:
+			initSimultaneous();
+			break;
+		}
+
+		while (goOn && !isInterrupted()) {
+			if (numBlink > 0) {
+				if (round++ >= numBlink)
+					goOn = false;
+			}
+			
+			try {
+				Thread.sleep(delay);
+			} catch (InterruptedException e) {
+				// interrupted while sleeping
+				goOn = false;
+				interrupt();
+			}
+
+			ledState12 = toggle(ledState12);
+			ledState13 = toggle(ledState13);
+
+			motorSensor.getMotor().setLed12State(ledState12);
+			motorSensor.getMotor().setLed13State(ledState13);
+		}
+		
+		// Befor exiting thread turn off leds
+		motorSensor.getMotor().setLed12State(LedState.OFF);
+		motorSensor.getMotor().setLed13State(LedState.OFF);
+	}
+
+	/**
+	 * ON --> OFF and OFF --> ON
+	 * 
+	 * @param ledState
+	 *            Input led state
+	 * @return Toggled led state
+	 */
+	private LedState toggle(LedState ledState) {
+		return (ledState == LedState.ON) ? LedState.OFF : LedState.ON;
+	}
+
+	/**
+	 * Initialize with both leds on
+	 */
+	private void initSimultaneous() {
+		ledState12 = LedState.ON;
+		ledState13 = LedState.ON;
+	}
+
+	/**
+	 * Initialize with one led on and the other off
+	 */
+	private void initAlternating() {
+		ledState12 = LedState.ON;
+		ledState13 = LedState.OFF;
+	};
+
+}
--- raspi/src/main/java/de/loosensimnetz/iot/raspi/motor/MotorSensorMonitor.java
+++ raspi/src/main/java/de/loosensimnetz/iot/raspi/motor/MotorSensorMonitor.java
@@ -3,61 +3,139 @@ package de.loosensimnetz.iot.raspi.motor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.loosensimnetz.iot.raspi.motor.BlinkingLed.BlinkType;
+import de.loosensimnetz.iot.raspi.motor.Motor.LedState;
+import de.loosensimnetz.iot.raspi.motor.state.MotorStateError;
+import de.loosensimnetz.iot.raspi.motor.state.MotorStateMovingDown;
+import de.loosensimnetz.iot.raspi.motor.state.MotorStateMovingUp;
+import de.loosensimnetz.iot.raspi.motor.state.MotorStateStoppedDown;
+import de.loosensimnetz.iot.raspi.motor.state.MotorStateStoppedInitial;
+
 /**
  * Monitor daemon thread class
  * 
- * Monitors the {@link MotorSensor} and calls update() for state updates in regular intervals
+ * Monitors the {@link MotorSensor} and calls update() for state updates in
+ * regular intervals
  * 
  * @author jloosen
  *
  */
 public class MotorSensorMonitor extends Thread {
 	private final long updateInterval;
-	private final MotorSensor sensor;
+	private final MotorSensor motorSensor;
 	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	private BlinkingLed blinkingLed = null;
 
 	/**
 	 * Constructor
 	 * 
 	 * Sets the daemon proerty and descriptive name for the thread.
 	 * 
-	 * @param updateInterval Update interval in milliseconds
-	 * @param sensor The monitored {@link MotorSensor}
+	 * @param updateInterval
+	 *            Update interval in milliseconds
+	 * @param motorSensor
+	 *            The monitored {@link MotorSensor}
 	 */
-	public MotorSensorMonitor(long updateInterval, MotorSensor sensor) {
+	public MotorSensorMonitor(long updateInterval, MotorSensor motorSensor) {
 		super();
 		this.updateInterval = updateInterval;
-		this.sensor = sensor;
+		this.motorSensor = motorSensor;
 		this.setDaemon(true);
 		this.setName("de.loosensimnetz.iot.raspi.motor.MotorSensorMonitor daemon thread");
 		this.setPriority(MIN_PRIORITY);
 	}
 
 	/**
-	 * Call update() method on the {@link MotorSensor} object in regular intervals approx.
-	 * every uptadeInterval milliseconds
+	 * MotorSensor thread loop.
+	 * 
+	 * 1.) Initialize the motor, so the two leds are off.
+	 * 2.) Call update() method on the {@link MotorSensor} object in regular intervals
+	 * 3.) If an error state is entered, start the BlinkingLed thread and let the leds blink three times
+	 * 4.) If an error state is left, interrupt (stop) the BlinkingLed thread.
+	 * 5.) 5.) If the motor is moving, turn on the respective led:
+					moving up   -> led12 OFF	led13 ON				
+					moving down -> led12 ON	    led13 OFF	
+	 * 6.) If the motor is stopped, turn off the leds
+	 * 
+	 * approx. every uptadeInterval milliseconds
 	 */
 	@Override
 	public void run() {
 		boolean goOn = true;
-		
+
 		logger.info("MotorSensorMonitor started");
+		
+		// Initialize leds
+		motorSensor.getMotor().setLed12State(LedState.OFF);
+		motorSensor.getMotor().setLed13State(LedState.OFF);
 
 		while (goOn && !this.isInterrupted()) {
 			logger.debug("Delaying for {} milliseconds", updateInterval);
 
 			try {
-				String oldState = sensor.getStateId();
+				String oldState = motorSensor.getStateId();
 				Thread.sleep(updateInterval);
-				sensor.update(System.currentTimeMillis());
-				String newState = sensor.getStateId();
-				
+				motorSensor.update(System.currentTimeMillis());
+				String newState = motorSensor.getStateId();
+
 				if (!oldState.equals(newState)) {
 					logger.info("Updating state. Old state: {}, new state: {}", oldState, newState);
+
+					//  3.) If an error state is entered, start the BlinkingLed thread and let the leds blink three times
+					if (motorSensor.getState() == MotorStateError.instance() && blinkingLed == null) {
+						logger.info("Error state - starting to blink.");
+
+						blinkingLed = new BlinkingLed(3, 500L, BlinkType.ALTERNATING, motorSensor);
+						blinkingLed.start();
+					}
+					
+					// 4.) If an error state is left, interrupt (stop) the BlinkingLed thread.
+					if (motorSensor.getState() != MotorStateError.instance() && blinkingLed != null) {
+						logger.info("Error state left - stopping to blink.");
+
+						blinkingLed.interrupt();
+						blinkingLed = null;
+					}
+					
+					// 5.) If the motor is moving, turn on the respective led:
+					// 			moving up   -> led12 OFF	led13 ON				
+					// 			moving down -> led12 ON	    led13 OFF					
+					if (motorSensor.getState() != MotorStateMovingUp.instance()) {
+						logger.info("Entered MotorStateMovingDown. Turning on led 1.");
+
+						motorSensor.getMotor().setLed12State(LedState.ON);
+						motorSensor.getMotor().setLed13State(LedState.OFF);
+					}
+					
+					if (motorSensor.getState() != MotorStateMovingDown.instance()) {
+						logger.info("Entered MotorStateMovingDown. Turning on led 2.");
+
+						motorSensor.getMotor().setLed12State(LedState.OFF);
+						motorSensor.getMotor().setLed13State(LedState.ON);
+					}
+					
+					// 6.) If the motor is stopped, turn off the leds
+					if (motorSensor.getState() != MotorStateStoppedDown.instance() ||
+						motorSensor.getState() != MotorStateStoppedInitial.instance()) {
+						
+						logger.info("Entered MotorStateStoppedDown or MotorStateStoppedInitial. Turning off leds.");
+
+						motorSensor.getMotor().setLed12State(LedState.OFF);
+						motorSensor.getMotor().setLed13State(LedState.OFF);
+					}
 				}
+
 			} catch (InterruptedException e) {
 				logger.error("MotorSensorMonitor interrupted - exiting", e);
 				goOn = false;
+				
+				// If we are blinking - interrupt (stop) the thread
+				if (blinkingLed != null) {
+					blinkingLed.interrupt();
+					blinkingLed = null;
+				}
+				
 				this.interrupt();
 			}
 		}
